502815a8a46bdff0c2e8606a060d68c947512608
pllucian <pllucian@student.21-school.ru>
2020-11-13 13:10:49 +0300

    ft_strchr

--- Makefile
+++ Makefile
@@ -6,12 +6,12 @@
 #    By: pllucian <pllucian@21-school.ru>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/11/10 10:52:07 by pllucian          #+#    #+#              #
-#    Updated: 2020/11/11 22:33:40 by pllucian         ###   ########.fr        #
+#    Updated: 2020/11/13 11:46:20 by pllucian         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 SRCS =	ft_memset.c ft_bzero.c ft_memcpy.c ft_memccpy.c ft_memmove.c ft_memchr.c ft_memcmp.c	\
-		ft_strlen.c ft_strlcpy.c ft_strlcat.c
+		ft_strlen.c ft_strlcpy.c ft_strlcat.c ft_strchr.c
 
 OBJS = $(SRCS:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pllucian <pllucian@21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 11:23:35 by pllucian          #+#    #+#             */
-/*   Updated: 2020/11/11 22:33:46 by pllucian         ###   ########.fr       */
+/*   Updated: 2020/11/13 12:58:18 by pllucian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
+char	*ft_strchr(const char *s, int c);
 
 #endif
