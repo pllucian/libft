a8dcc8a36f6294510ebb405ad21e0917d2ae3e2c
pllucian <pllucian@student.21-school.ru>
2020-11-15 17:26:14 +0300

    ft_isascii

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pllucian <pllucian@21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 11:23:35 by pllucian          #+#    #+#             */
-/*   Updated: 2020/11/15 17:12:00 by pllucian         ###   ########.fr       */
+/*   Updated: 2020/11/15 17:24:05 by pllucian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,6 @@ int		ft_atoi(const char *nptr);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
+int		ft_isascii(int c);
 
 #endif
