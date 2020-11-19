b944c2802a380912c823c50d3ce0fad91d8c5694
pllucian <pllucian@student.21-school.ru>
2020-11-19 16:34:37 +0300

    ft_strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pllucian <pllucian@21-school.ru>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/19 16:17:30 by pllucian          #+#    #+#             */
+/*   Updated: 2020/11/19 16:31:44 by pllucian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	size_t	i;
+
+	if (s && f && (str = (char*)malloc(sizeof(char) * (ft_strlen(s) + 1))))
+	{
+		i = 0;
+		while (s[i])
+		{
+			str[i] = f(i, s[i]);
+			i++;
+		}
+		str[i] = '\0';
+	}
+	return (str);
+}
