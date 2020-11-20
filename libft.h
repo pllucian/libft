0cd4641734057866cbfc1f4758764eb876a187a6
pllucian <pllucian@student.21-school.ru>
2020-11-21 00:16:29 +0300

    ft_lstsize

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pllucian <pllucian@21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 11:23:35 by pllucian          #+#    #+#             */
-/*   Updated: 2020/11/20 23:55:37 by pllucian         ###   ########.fr       */
+/*   Updated: 2020/11/21 00:05:57 by pllucian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,5 +61,6 @@ typedef struct		s_list
 
 t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
+int					ft_lstsize(t_list *lst);
 
 #endif
